ca4a20a5f5c3ae415724eb0776ab0afa1c099d7e
nadasaid355 <nsaid8393@gmail.com>
2023-03-17 17:28:03 -0700

    Task4: Alphabt~~

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,13 +9,13 @@
  */
 int main(void)
 {
-	int  i;
+	int i;
 
-	for (i = 97; i <= 123; i++)
+	for (i = 97; i < 123; i++)
 	{
 		if (i != 101 && i != 113)
 		{
-		       putchar(i);
+			putchar(i);
 		}
 	}
 	putchar('\n');
