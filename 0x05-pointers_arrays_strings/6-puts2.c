ff1beefac047dc2eac9dc7cbb930c2ae47101af2
nadasaid355 <nsaid8393@gmail.com>
2023-03-27 17:31:00 -0700

    Task6: Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * puts - function should print only one character out of two
+ * puts2 - function should print only one character out of two
  * starting with the first one
  * @str: input
  * Return: print
