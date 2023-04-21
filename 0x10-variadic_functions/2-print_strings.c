3505c6cea97129c9a3bbc267bd45bce356d75d48
nadasaid355 <nsaid8393@gmail.com>
2023-04-20 17:40:08 -0700

    2-print_strings.c

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -23,6 +23,8 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		str = va_arg(strings, char *);
 		if (str == NULL)
 			printf("(nil)");
+		else
+			print("%s", str);
 		if (index != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
